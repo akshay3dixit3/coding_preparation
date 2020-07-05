640c4ed0751ca953956de385e3bcdc9e7a00c0ef
Akshay Dixit <48876948+akshay3dixit3@users.noreply.github.com>
2020-07-05 19:26:19 +0530

    Update Prison Cells After N Days.cpp

--- leet_code_july_challange/week1/Prison Cells After N Days.cpp
+++ leet_code_july_challange/week1/Prison Cells After N Days.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
   vector<int> prisonAfterNDays(vector<int>& c, int N) {
-  vector<int> f_c, next_c(c.size(), 0);
-  for (int cycle = 0; N-- > 0; c = next_c, ++cycle) {
-    for (auto i = 1; i < c.size() - 1; ++i) next_c[i] = c[i - 1] == c[i + 1];
-    if (cycle == 0) f_c = next_c;
-    else if (next_c == f_c) N %= cycle;
+    
+  vector<int> first_combination, next_combination(c.size(), 0);
+    
+  for (int mod_value = 0; N-- > 0; c = next_combination, ++mod_value) {
+    for (auto i = 1; i < c.size() - 1; ++i) next_combination[i] = c[i - 1] == c[i + 1];
+    
+    if (mod_value == 0) first_combination = next_combination;
+    else if (next_combination == first_combination) N %= mod_value;
+  
   }
   return c;
+    
 }
 };
